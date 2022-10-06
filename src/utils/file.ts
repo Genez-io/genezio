e2a2c8490ba2fbdb2bef6f1ce5bdedcb39f74f7c
Radu Dumitrescu <r.dumitrescu@reply.com>
2022-10-06 21:38:47 +0300

    added get all non js files

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,81 +1,115 @@
-import { rejects } from 'assert';
-import fs from 'fs';
+import { rejects } from "assert";
+import fs from "fs";
 import os from "os";
 import path from "path";
-import FileDetails from '../models/fileDetails';
+import FileDetails from "../models/fileDetails";
+import glob from "glob";
 
+export async function getAllNonJsFiles(): Promise<FileDetails[]> {
+  return new Promise((resolve, reject) => {
+    const cwd = process.cwd();
+    glob(`${cwd}/**/*`, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+
+      const fileDetails: FileDetails[] = files
+        .filter((file: string) => {
+          return path.extname(file) !== ".js";
+        })
+        .map((file: string) => {
+          return {
+            name: path.parse(file).name,
+            extension: path.parse(file).ext,
+            path: file,
+            filename: file
+          };
+        });
+      resolve(fileDetails);
+    });
+  });
+}
 
 export async function fileExists(filePath: string): Promise<boolean> {
-    return new Promise((resolve) => {
-        fs.stat(filePath, (exists) => {
-            if (exists == null) {
-                return resolve(true);
-            } else if (exists.code === 'ENOENT') {
-                return resolve(false);
-            }
-        });
+  return new Promise((resolve) => {
+    fs.stat(filePath, (exists) => {
+      if (exists == null) {
+        return resolve(true);
+      } else if (exists.code === "ENOENT") {
+        return resolve(false);
+      }
     });
+  });
 }
 
 export async function createTemporaryFolder(name: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        fs.mkdtemp(path.join(os.tmpdir(), 'foo-'), (error: any, folder: string) => {
-            if (error) {
-                rejects(error)
-            }
+  return new Promise((resolve, reject) => {
+    fs.mkdtemp(path.join(os.tmpdir(), "foo-"), (error: any, folder: string) => {
+      if (error) {
+        rejects(error);
+      }
 
-            resolve(folder)
-        });
+      resolve(folder);
     });
+  });
 }
 
 export function getFileDetails(filePath: string): FileDetails {
-    const { ext, name, dir, base } = path.parse(filePath);
+  const { ext, name, dir, base } = path.parse(filePath);
 
-    return new FileDetails(name, ext, dir, base);
+  return new FileDetails(name, ext, dir, base);
 }
 
 export function readUTF8File(filePath: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        fs.readFile('./genezio.yaml', 'utf8', function (error, data) {
-            if (error) {
-                reject(error)
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile("./genezio.yaml", "utf8", function (error, data) {
+      if (error) {
+        reject(error);
+      }
 
-            resolve(data)
-        })
-    })
+      resolve(data);
+    });
+  });
 }
 
-export function writeToFile(folderPath: string, filename: string, content: any, createPathIfNeeded: boolean = false): Promise<void> {
-    return new Promise((resolve, reject) => {
-        if (!fs.existsSync(folderPath) && createPathIfNeeded) {
-            fs.mkdirSync(folderPath);
-        }
-    
-        fs.writeFile(path.join(folderPath, filename), content, function (error) {
-            if (error) {
-                reject(error)
-                return;
-            }
-    
-            resolve();
-        })
-    })
+export function writeToFile(
+  folderPath: string,
+  filename: string,
+  content: any,
+  createPathIfNeeded: boolean = false
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(folderPath) && createPathIfNeeded) {
+      fs.mkdirSync(folderPath);
+    }
+
+    fs.writeFile(path.join(folderPath, filename), content, function (error) {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      resolve();
+    });
+  });
 }
 
 export async function writeToken(token: string) {
-    return writeToFile(os.homedir(), ".genezio", token, true);
+  return writeToFile(os.homedir(), ".genezio", token, true);
 }
 
 export async function readToken(): Promise<string> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path.join(os.homedir(), ".genezio"), 'utf8', function (error, data) {
-            if (error) {
-                reject(error)
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(os.homedir(), ".genezio"),
+      "utf8",
+      function (error, data) {
+        if (error) {
+          reject(error);
+        }
 
-            resolve(data)
-        })
-    })
+        resolve(data);
+      }
+    );
+  });
 }
