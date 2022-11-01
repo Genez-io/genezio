3426f4f88d49cecbe1dec56ce9ed6c840a3ae0f1
Bogdan Vlad <bogdan@genez.io>
2022-11-01 12:56:58 +0200

    Add example for webhook use case.

---
+++ examples/webhook/index.js
@@ -0,0 +1,27 @@
+export class HelloWorldCronExample {
+    helloWorld(name) {
+        return {
+            message: `Hello, ${name}!`
+        }
+    }
+
+    helloWorldOverHttp(request) {
+        const name = JSON.parse(request.body).name
+
+        console.log(name)
+
+        return {
+            body: JSON.stringify({
+                name,
+            }),
+            bodyEncoding: 'text',
+            // headers: {
+            //     "testHeader": [{
+            //         "testHeader": "testHeaderValue",
+            //      }],
+            // },
+            status: '200',
+            statusDescription: 'Ok'
+        };
+    }
+}
