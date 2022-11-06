4481921e1f66146cb8dd2499114934e3e690a7a4
Bogdan Vlad <bogdan@genez.io>
2022-11-06 17:59:50 +0100

    Change back variables.

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,5 @@
 export const REACT_APP_BASE_URL = "https://app.genez.io";
-export const BACKEND_ENDPOINT = "http://127.0.0.1:8080";
+export const BACKEND_ENDPOINT = "https://api.genez.io";
 export const GENERATE_SDK_API_URL = "https://sdk-api.genez.io";
 export const PORT = 8000;
 export const PORT_LOCAL_ENVIRONMENT = 8083;
