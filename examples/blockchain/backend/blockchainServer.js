a494dce2ab5a0e22ff9d836fea6b7b9261914790
Bogdan Vlad <bogdan@genez.io>
2022-11-08 11:10:26 +0100

    Remove todo list SDK from git.

--- examples/blockchain/backend/blockchainServer.js
+++ examples/blockchain/backend/blockchainServer.js
@@ -4,6 +4,7 @@ import { EventModel } from "./models/event.js"
 import { mongoose } from "mongoose";
 
 
+// Replace these with your own values.
 const MONGO_DB_URI = "mongodb+srv://genezio:genezio@cluster0.c6qmwnq.mongodb.net/?retryWrites=true&w=majority"
 const CONTRACT_ADDRESS = "0x942BC2d3e7a589FE5bd4A5C6eF9727DFd82F5C8a"
 const BWARE_URL = "https://eth-mainnet.blastapi.io/23450296-544f-4ccc-a438-7cf90659b49e"
