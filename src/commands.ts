1dc068337089812f19edbc6c5e5f09b318d04750
Virgil993 <80636398+Virgil993@users.noreply.github.com>
2022-11-15 12:50:03 +0200

    Fix path delimiter for Windows and Unix. (#31)
    * Fix path delimiter for Windows and Unix.
    
    Signed-off-by: Virgil993 <turcu.virgil@gmail.com>

--- src/commands.ts
+++ src/commands.ts
@@ -93,13 +93,12 @@ export async function getNodeModules(filePath: string): Promise<any> {
         reject(err);
         return;
       }
-
       const dependenciesInfo = dependencies.map((dependency) => {
-        const relativePath = dependency.split("node_modules/")[1];
+        const relativePath = dependency.split("node_modules" + path.sep)[1];
         const dependencyName = relativePath?.split(path.sep)[0];
         const dependencyPath =
-          dependency.split("node_modules/")[0] +
-          "node_modules/" +
+          dependency.split("node_modules" + path.sep)[0] +
+          "node_modules" + path.sep +
           dependencyName;
         return {
           name: dependencyName,
