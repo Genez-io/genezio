60500aa53ffbf345ed7a3f1146fe1e1601be1913
Virgil993 <turcu.virgil@gmail.com>
2022-11-22 13:45:59 +0200

    changed variables to dev

--- src/variables.ts
+++ src/variables.ts
@@ -1,8 +1,3 @@
-// export const REACT_APP_BASE_URL = "https://app.genez.io";
-// export const BACKEND_ENDPOINT = "https://api.genez.io";
-// export const GENERATE_SDK_API_URL = "https://sdk-api.genez.io";
-
-
 export const REACT_APP_BASE_URL = "https://dev.app.genez.io";
 export const BACKEND_ENDPOINT = "http://localhost:8080";
 export const GENERATE_SDK_API_URL = "https://dev-sdk-api.genez.io";
