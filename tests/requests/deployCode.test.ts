0d937b0ea57d81e01da5034f6e139486dc1be940
Bogdan Vlad <bogdan@genez.io>
2022-12-01 15:48:08 +0200

    Fix unit tests.

--- tests/requests/deployCode.test.ts
+++ tests/requests/deployCode.test.ts
@@ -4,19 +4,19 @@ import { ClassConfiguration, TriggerType } from '../../src/models/projectConfigu
 import { deployClass } from "../../src/requests/deployCode"
 
 jest.mock('axios');
-jest.mock("../../src/utils/file");
+jest.mock("../../src/utils/accounts");
 
 const mockedAxios = jest.mocked(axios, { shallow: true })
-const mockedReadToken = jest.mocked(getAuthToken, { shallow: true })
+const mockedGetAuthToken = jest.mocked(getAuthToken, { shallow: true })
 
 beforeEach(() => {
-    mockedReadToken.mockClear()
+    mockedGetAuthToken.mockClear()
 })
 
 test('should throw error if server returns error', async () => {
     await expect(async () => {
         const classConfiguration = new ClassConfiguration("./test", TriggerType.cron, "js", [])
-        mockedReadToken.mockResolvedValue("token")
+        mockedGetAuthToken.mockResolvedValue("token")
         mockedAxios.mockResolvedValue({ data: { status: "error" }, status: 200, statusText: "Ok", headers: {}, config: {} });
 
         await deployClass(classConfiguration, 'test', 'test', "test")
@@ -26,7 +26,7 @@ test('should throw error if server returns error', async () => {
 test('should throw error if server returns data.error object', async () => {
     await expect(async () => {
         const classConfiguration = new ClassConfiguration("./test", TriggerType.cron, "js", [])
-        mockedReadToken.mockResolvedValue("token")
+        mockedGetAuthToken.mockResolvedValue("token")
         mockedAxios.mockResolvedValue({ data: { error: { message: "error text" } }, status: 200, statusText: "Ok", headers: {}, config: {} });
 
         await deployClass(classConfiguration, 'test', 'test', "test")
@@ -36,7 +36,7 @@ test('should throw error if server returns data.error object', async () => {
 test('should return response.data if everything is ok', async () => {
     const someObject = { someData: "data" }
     const classConfiguration = new ClassConfiguration("./test", TriggerType.cron, "js", [])
-    mockedReadToken.mockResolvedValue("token")
+    mockedGetAuthToken.mockResolvedValue("token")
     mockedAxios.mockResolvedValue({ data: someObject, status: 200, statusText: "Ok", headers: {}, config: {} });
 
     const response = await deployClass(classConfiguration, 'test', 'test', "test")
@@ -46,12 +46,12 @@ test('should return response.data if everything is ok', async () => {
 test('should read token and pass it to headers', async () => {
     const someObject = { someData: "data" }
     const classConfiguration = new ClassConfiguration("./test", TriggerType.cron, "js", [])
-    mockedReadToken.mockResolvedValue("token")
+    mockedGetAuthToken.mockResolvedValue("token")
     mockedAxios.mockResolvedValue({ data: someObject, status: 200, statusText: "Ok", headers: {}, config: {} });
 
     const response = await deployClass(classConfiguration, 'test', 'test', "test")
 
-    expect(mockedReadToken.mock.calls.length).toBe(1)
+    expect(mockedGetAuthToken.mock.calls.length).toBe(1)
 
     expect(response).toEqual(someObject)
 });
