ef8e6d00118a90abf3cd0e3f305309f14d3c66c5
Radu Dumitrescu <r.dumitrescu@reply.com>
2022-12-29 15:38:58 +0200

    adding comments

--- examples/webhook/server/index.js
+++ examples/webhook/server/index.js
@@ -5,6 +5,14 @@ export class HelloWorldCronExample {
     };
   }
 
+  /**
+   * @param request
+   * @param request.headers - request headers
+   * @param request.http - http method (GET, POST, etc)
+   * @param request.queryParameters - query parameters from the request URL
+   * @param request.timeEpoch - time in milliseconds since epoch
+   * @param request.body - your request body
+  */
   helloWorldOverHttp(request) {
     console.log("Request received!")
     const name = request.body ? JSON.parse(request.body).name : "No name";
