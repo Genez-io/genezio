5b2c5165ff3e8400ee35574edeec675c6ec22155
MagoDelBlocco <bgdnkln@gmail.com>
2023-01-17 15:00:40 +0200

    Remove setting env variables in CLI

--- solveEnvVariables.js
+++ solveEnvVariables.js
@@ -2,6 +2,14 @@ var envify = require("envify")
     , fs = require('fs')
 
 var data = ""
+
+if (process.argv.length == 4) {
+    process.env.NODE_ENV = process.argv[3];
+} else {
+    process.env.NODE_ENV = "production";
+}
+console.log("Environment installed: " + process.env.NODE_ENV);
+
 fs.createReadStream(process.argv[2], { encoding: 'utf8' })
     .pipe(envify(process.argv[2]))
     .on("data", (chunk) => {
