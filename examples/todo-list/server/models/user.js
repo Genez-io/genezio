bc7ca7a508a014a5efcb4a16660e7709c657bf35
Andreia Ocanoaia <andreia@genez.io>
2023-02-08 22:04:36 +0200

    Remove examples dir
    Signed-off-by: Andreia Ocanoaia <andreia@genez.io>

--- examples/todo-list/server/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-  });
-
-export const UserModel = mongoose.model('User', userSchema);
