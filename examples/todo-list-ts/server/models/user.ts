9b590375639c8cb48ebda7ba3c1564d2915dfb99
Andreia Ocanoaia <andreia@genez.io>
2023-02-08 18:57:38 +0200

    Remove examples from repo
    Signed-off-by: Andreia Ocanoaia <andreia@genez.io>

--- examples/todo-list-ts/server/models/user.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-  });
-
-export const UserModel = mongoose.model('User', userSchema);
