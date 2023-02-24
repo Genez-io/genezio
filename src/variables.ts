fff76e2571be0cd35e3586a307a4ee876027a364
MagoDelBlocco <bgdnkln@gmail.com>
2023-02-24 15:58:02 +0200

    Remove leftovers from variables

--- src/variables.ts
+++ src/variables.ts
@@ -12,16 +12,14 @@ let PUPPETEER_DATA_DIR: string;
 if (environment === "dev") {
     REACT_APP_BASE_URL = "https://dev.app.genez.io";
     FRONTEND_DOMAIN = "dev.app.genez.io";
-//    BACKEND_ENDPOINT = "https://dev.api.genez.io";
-	BACKEND_ENDPOINT = "http://127.0.0.1:8080";
+    BACKEND_ENDPOINT = "https://dev.api.genez.io";
     GENERATE_SDK_API_URL = "https://dev-sdk-api.genez.io";
     LOCAL_TEST_INTERFACE_URL = "https://dev.app.genez.io/test-interface/local";
     PORT_LOCAL_ENVIRONMENT = 8083;
     ENABLE_DEBUG_LOGS_BY_DEFAULT = true;
     // Needed by the alternate login method. You can get your by entering "chrome://version"
     // in your chromium-based browser. The path is under "Profile Path". Remove the `/Default` part.
-    PUPPETEER_DATA_DIR = "/home/ghidorah/.config/google-chrome";
-    //PUPPETEER_DATA_DIR = "C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data";
+    PUPPETEER_DATA_DIR = "";
 } else {
     REACT_APP_BASE_URL = "https://app.genez.io";
     FRONTEND_DOMAIN = "app.genez.io";
