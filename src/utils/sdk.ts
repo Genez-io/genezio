7e75b8fe9de208a434183bb02c722fd55cc073dc
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-03-24 13:49:51 +0200

    console log fix

--- src/utils/sdk.ts
+++ src/utils/sdk.ts
@@ -13,9 +13,6 @@ export type ClassUrlMap = {
  * Replace the temporary markdowns from the SDK with actual URLs.
  */
 export async function replaceUrlsInSdk(sdkResponse: SdkGeneratorResponse, classUrlMap: ClassUrlMap[]) {
-    console.log("Replacing the temporary markdowns with actual URLs...")
-    console.log("sdkResponse", sdkResponse)
-    console.log("classUrlMap", classUrlMap)
     sdkResponse.files.forEach((c : SdkFileClass) => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         const classContent = classUrlMap.find((classFile) => {
