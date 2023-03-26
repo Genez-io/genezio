259cacc58f9a2369d9f01f09dd109ba334b89818
Bogdan Vlad <bogdan@genez.io>
2023-03-26 12:46:36 +0300

    Rename variable and add a few comments.

--- src/bundlers/javascript/nodeJsLocalBundler.ts
+++ src/bundlers/javascript/nodeJsLocalBundler.ts
@@ -1,7 +1,9 @@
 import { writeToFile } from "../../utils/file";
 import { BundlerInput, BundlerInterface, BundlerOutput } from "../bundler.interface";
 
-const test = `
+// This file is the wrapper that is used to run the user's code in a separate process.
+// It listens for messages from the parent process and runs the user's code when it receives a message.
+const localWrapperCode = `
 const userHandler = require("./index.js")
 
 process.on('message', (msg) => {
@@ -18,9 +20,10 @@ function wait() {
 wait()
 `
 
+// Adds a wrapper to the user's code that allows it to be run in a separate process.
 export class NodeJsLocalBundler implements BundlerInterface {
     async bundle(input: BundlerInput): Promise<BundlerOutput> {
-        await writeToFile(input.path, "local.js", test, true)
+        await writeToFile(input.path, "local.js", localWrapperCode, true)
 
         return input
     }
