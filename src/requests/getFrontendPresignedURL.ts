d9c8e777273dcdfe05d0d6387a9c948178e85402
stefandaius <stefan.iordache2001@gmail.com>
2023-06-29 12:45:57 +0300

    catch error for existing subdomain

--- src/requests/getFrontendPresignedURL.ts
+++ src/requests/getFrontendPresignedURL.ts
@@ -3,50 +3,53 @@ import { getAuthToken } from "../utils/accounts.js";
 import { BACKEND_ENDPOINT } from "../constants.js";
 import { GENEZIO_NOT_AUTH_ERROR_MSG } from "../errors.js";
 import { createRequire } from 'module';
+import { AxiosError } from "axios";
 const requireESM = createRequire(import.meta.url);
 
 const pjson = requireESM("../../package.json");
 
 export async function getFrontendPresignedURL(
-    subdomain: string,
-    projectName: string
+  subdomain: string,
+  projectName: string
 ) {
-    const region = "us-east-1";
-    if (!subdomain || !projectName) {
-        throw new Error("Missing required parameters");
-    }
+  const region = "us-east-1";
+  if (!subdomain || !projectName) {
+    throw new Error("Missing required parameters");
+  }
 
-    // Check if user is authenticated
-    const authToken = await getAuthToken()
-    if (!authToken) {
-        throw new Error(GENEZIO_NOT_AUTH_ERROR_MSG);
-    }
+  // Check if user is authenticated
+  const authToken = await getAuthToken()
+  if (!authToken) {
+    throw new Error(GENEZIO_NOT_AUTH_ERROR_MSG);
+  }
 
-    const json = JSON.stringify({
-        subdomainName: subdomain,
-        projectName: projectName,
-        region: region,
-    });
+  const json = JSON.stringify({
+    subdomainName: subdomain,
+    projectName: projectName,
+    region: region,
+  });
 
-    const response: any = await axios({
-        method: "GET",
-        url: `${BACKEND_ENDPOINT}/core/frontend-deployment-url`,
-        data: json,
-        headers: {
-            Authorization: `Bearer ${authToken}`,
-            "Accept-Version": `genezio-cli/${pjson.version}`
-        },
-        maxContentLength: Infinity,
-        maxBodyLength: Infinity
-    })
+  const response: any = await axios({
+    method: "GET",
+    url: `${BACKEND_ENDPOINT}/core/frontend-deployment-url`,
+    data: json,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      "Accept-Version": `genezio-cli/${pjson.version}`
+    },
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity
+  }).catch((error: AxiosError) => {
+    throw new Error((error.response?.data as any).error.message)
+  });
 
-    if (response.data.status === "error") {
-        throw new Error(response.data.message);
-    }
+  if (response.data.status === "error") {
+    throw new Error(response.data.message);
+  }
 
-    if (response.data?.error?.message) {
-        throw new Error(response.data.error.message);
-    }
+  if (response.data?.error?.message) {
+    throw new Error(response.data.error.message);
+  }
 
-    return response.data;
+  return response.data;
 }
