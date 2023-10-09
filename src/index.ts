247f0e888bb541fc13995a7390803db4b2f86896
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-10-09 23:26:46 +0300

    added sentry tracker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@
 import program from "./genezio.js";
 import { GenezioTelemetry, TelemetryEventTypes } from "./telemetry/telemetry.js";
 import { cleanupTemporaryFolders } from "./utils/file.js";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+import { SENTRY_DSN } from "./constants.js";
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+});
 
 
 // Set-up SIGINT and exit handlers that clean up the temporary folder structure
