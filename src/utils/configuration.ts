2db9d2c518a89443a5de10298113dabdd259e78c
Bogdan Vlad <bogdan@genez.io>
2023-10-24 09:17:55 +0300

    Code review

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -14,8 +14,7 @@ import babel from '@babel/core';
 import fs from "fs"
 import FileDetails from "../models/fileDetails.js";
 import { debugLogger } from "./logging.js";
-import { error } from "console";
-import { exit } from "process";
+
 
 type MethodDecoratorInfo = {
     name: string,
@@ -135,6 +134,7 @@ async function tryToReadClassInformationFromDecorators(projectConfiguration: Yam
         return getDecoratorsFromFile(file.path)
             .catch((error)=>{ 
                 if (error.reasonCode == "MissingOneOfPlugins") {
+                    debugLogger.error(`Error while parsing the file ${file.path}`, error) 
                     return []
                 } else {
                     throw error
