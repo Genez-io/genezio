51166f3ce88375d39f921dc8d94006e01ceec605
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-11-06 11:22:25 +0200

    wrap sentry with try catch

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,29 @@
 import program from "./genezio.js";
 import { GenezioTelemetry, TelemetryEventTypes } from "./telemetry/telemetry.js";
 import { cleanupTemporaryFolders } from "./utils/file.js";
-import * as Sentry from "@sentry/node";
-import { ProfilingIntegration } from "@sentry/profiling-node";
 import { SENTRY_DSN } from "./constants.js";
+import { debugLogger } from "./utils/logging.js";
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  integrations: [
-    new ProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-});
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const Sentry = await import("@sentry/node");
+  const { ProfilingIntegration } = await import ("@sentry/profiling-node");
+
+  Sentry?.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  });
+} catch (error) {
+  debugLogger.debug("Sentry not initialized", error);
+}
+
 
 // Set-up SIGINT and exit handlers that clean up the temporary folder structure
 process.on('SIGINT', async () => {
