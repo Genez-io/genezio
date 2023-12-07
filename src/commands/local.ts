04cdeb9eb4ff9fbb56f25637f6dda016d169f721
Virgil993 <turcu.virgil@gmail.com>
2023-12-07 16:19:19 +0200

    Used dockerode instead of exec

--- src/commands/local.ts
+++ src/commands/local.ts
@@ -57,6 +57,7 @@ import { compileSdk } from "../generateSdk/utils/compileSdk.js";
 import { runNewProcess } from "../utils/process.js";
 import { exit } from "process";
 import { promisify } from "util";
+import Docker, { Container, ContainerInfo } from "dockerode";
 const asyncExec = promisify(exec);
 
 import { getLinkPathsForProject } from "../utils/linkDatabase.js";
@@ -629,11 +630,22 @@ function getBundler(classConfiguration: ClassConfiguration): BundlerInterface | 
     return bundler;
 }
 
-async function runDockerContainer(databaseType: string, projectName: string, runCommand: string) {
-    const containerName = `genezio-${databaseType}-${projectName}`;
-    let output;
+async function runDockerDatabaseContainer(
+    dockerHandler: Docker,
+    databaseType: string,
+    projectName: string,
+) {
+    const containerName = `/genezio-${databaseType}-${projectName}`;
+    let currentContainer: Container | undefined;
     try {
-        output = await asyncExec(`docker ps -a --format "{{.Names}}" `);
+        const containers = await dockerHandler.listContainers({ all: true });
+        containers.forEach(function (container) {
+            if (container.Names[0] == containerName) {
+                currentContainer = dockerHandler.getContainer(container.Id);
+            }
+        });
+
+        // output = await asyncExec(`docker ps -a --format "{{.Names}}" `);
     } catch (error: any) {
         printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
         log.error(`An error has occured: ${error.toString()}`);
@@ -642,125 +654,150 @@ async function runDockerContainer(databaseType: string, projectName: string, run
         );
         return undefined;
     }
-    if (output) {
-        const containerNames = output.stdout.trim().split(`\n`);
-        if (containerNames.includes(containerName)) {
-            let isContainerRunningOutput;
+    if (currentContainer) {
+        const inspectRes = await currentContainer.inspect();
+        const isContainerRunning = inspectRes.State.Running;
+        if (!isContainerRunning) {
             try {
-                isContainerRunningOutput =
-                    await asyncExec(`docker inspect -f '{{.State.Running}}' ${containerName}
-                `);
-            } catch (error: any) {
-                printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
-                log.error(`An error has occured: ${error.toString()}`);
-                return undefined;
-            }
-            const isContainerRunning =
-                isContainerRunningOutput.stdout.trim().replaceAll("'", "") === "true";
-
-            if (!isContainerRunning) {
-                try {
-                    await asyncExec(`docker start ${containerName}
-                        `);
-                } catch (error: any) {
-                    printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
-                    log.error(`An error has occured: ${error.toString()}`);
-                    return undefined;
-                }
-            }
-        } else {
-            try {
-                await asyncExec(runCommand);
+                await currentContainer.start();
             } catch (error: any) {
                 printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
                 log.error(`An error has occured: ${error.toString()}`);
                 return undefined;
             }
         }
+    } else {
+        try {
+            switch (databaseType) {
+                case "postgres": {
+                    let isImagePresent = false;
+                    const images = await dockerHandler.listImages({ all: true });
+                    images.forEach(function (image) {
+                        if (image.RepoTags && image.RepoTags[0] == "postgres:latest") {
+                            isImagePresent = true;
+                        }
+                    });
+                    if (!isImagePresent) {
+                        const pullStream = await dockerHandler.pull("postgres");
+                        await new Promise((res) =>
+                            dockerHandler.modem.followProgress(pullStream, res),
+                        );
+                    }
+                    const newContainer = await dockerHandler.createContainer({
+                        Image: "postgres",
+                        Env: [`POSTGRES_PASSWORD=postgres`],
+                        HostConfig: {
+                            PortBindings: {
+                                "5432/tcp": [{ HostPort: "5432" }],
+                            },
+                            Binds: ["/root/dir:/tmp"],
+                        },
+                        ExposedPorts: {
+                            "5432/tcp": {},
+                        },
+                        name: containerName,
+                    });
+                    await newContainer.start();
+                    break;
+                }
+                case "redis": {
+                    let isImagePresent = false;
+                    const images = await dockerHandler.listImages({ all: true });
+                    images.forEach(function (image) {
+                        if (image.RepoTags && image.RepoTags[0] == "redis:latest") {
+                            isImagePresent = true;
+                        }
+                    });
+                    if (!isImagePresent) {
+                        const pullStream = await dockerHandler.pull("redis");
+                        await new Promise((res) =>
+                            dockerHandler.modem.followProgress(pullStream, res),
+                        );
+                    }
+                    const newContainer = await dockerHandler.createContainer({
+                        Image: "redis",
+                        HostConfig: {
+                            PortBindings: {
+                                "6379/tcp": [{ HostPort: "6379" }],
+                            },
+                            Binds: ["/root/dir:/tmp"],
+                        },
+                        ExposedPorts: {
+                            "6379/tcp": {},
+                        },
+                        name: containerName,
+                    });
+                    await newContainer.start();
+                    break;
+                }
+                default: {
+                    await stopDockerDatabase();
+                    printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
+                    log.error(
+                        "Unsupported database type, please use one of the following types of databases if you want to have a local testing db: [`postgres`,`redis`]",
+                    );
+                    break;
+                }
+            }
+        } catch (error: any) {
+            printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
+            log.error(`An error has occured: ${error.toString()}`);
+            return undefined;
+        }
     }
+
     printAdaptiveLog(`Completed starting local docker ${databaseType} database`, "end");
 }
 
 async function startDockerDatabase(database: YamlDatabaseConfiguration, projectName: string) {
+    let docker;
     try {
-        await asyncExec("docker --version");
+        docker = new Docker();
     } catch (error: any) {
         log.error("An error has occured: ", error.toString());
         log.error(
             "\x1b[33m%s\x1b[0m",
-            "Docker CLI not found, if you want to have a local database for testing, you need to install docker. Go to https://www.docker.com/products/docker-desktop/ to install docker",
+            "Docker not found, if you want to have a local database for testing, you need to install docker. Go to https://www.docker.com/products/docker-desktop/ to install docker",
         );
         printAdaptiveLog(`Failed starting local docker ${database.type} database`, "");
         return undefined;
     }
-
-    switch (database.type) {
-        case "postgres": {
-            await runDockerContainer(
-                "postgres",
-                projectName,
-                `docker run -p 5432:5432 --name genezio-postgres-${projectName} -e POSTGRES_PASSWORD=mysecretpassword -d postgres `,
-            );
-            log.info(
-                `Your local postgres database is up and running.\nYou can connect to your postgres instance using the following URL: postgres://postgres:mysecretpassword@localhost:5432`,
-            );
-            break;
-        }
-        case "redis": {
-            await runDockerContainer(
-                "redis",
-                projectName,
-                `docker run -p 6379:6379 --name genezio-redis-${projectName} -d redis `,
-            );
-            log.info(
-                `Your local redis database is up and running.\nYou can connect to the your redis instance using the following URL: redis://localhost:6379`,
-            );
-            break;
-        }
-        default: {
-            await stopDockerDatabase();
-            printAdaptiveLog(`Failed starting local docker ${database.type} database`, "");
-            log.error(
-                "Unsupported database type, please use one of the following types of databases if you want to have a local testing db: [`postgres`,`redis`]",
-            );
-            break;
-        }
-    }
+    await runDockerDatabaseContainer(docker, database.type, projectName);
 }
 
-async function stopDockerContainer(databaseType: string, projectName: string) {
-    const containerName = `genezio-${databaseType}-${projectName}`;
-    let output;
+async function stopDockerDatabaseContainer(
+    dockerHandler: Docker,
+    databaseType: string,
+    projectName: string,
+) {
+    const containerName = `/genezio-${databaseType}-${projectName}`;
+    let currentContainer: Container | undefined;
     try {
-        output = await asyncExec(`docker ps -a --format "{{.Names}}" `);
+        const containers = await dockerHandler.listContainers({ all: true });
+        containers.forEach(function (container) {
+            if (container.Names[0] == containerName) {
+                currentContainer = dockerHandler.getContainer(container.Id);
+            }
+        });
+
+        // output = await asyncExec(`docker ps -a --format "{{.Names}}" `);
     } catch (error: any) {
+        printAdaptiveLog(`Failed starting local docker ${databaseType} database`, "");
         log.error(`An error has occured: ${error.toString()}`);
         log.error(
             "This is most likely a docker error, if you want to solve it you can check out their documentation at https://docs.docker.com/ ",
         );
         return undefined;
     }
-    if (output) {
-        const containerNames = output.stdout.trim().split(`\n`);
-        if (containerNames.includes(containerName)) {
-            let isContainerRunningOutput;
+    if (currentContainer) {
+        const inspectRes = await currentContainer.inspect();
+        const isContainerRunning = inspectRes.State.Running;
+        if (isContainerRunning) {
             try {
-                isContainerRunningOutput =
-                    await asyncExec(`docker inspect -f '{{.State.Running}}' ${containerName}
-                `);
+                await currentContainer.stop();
             } catch (error: any) {
                 log.error(`An error has occured: ${error.toString()}`);
                 return undefined;
-            }
-            const isContainerRunning =
-                isContainerRunningOutput.stdout.trim().replaceAll("'", "") === "true";
-            if (isContainerRunning) {
-                try {
-                    await asyncExec(`docker stop ${containerName} `);
-                } catch (error: any) {
-                    log.error(`An error has occured: ${error.toString()}`);
-                    return undefined;
-                }
             }
         }
     }
@@ -769,31 +806,20 @@ async function stopDockerContainer(databaseType: string, projectName: string) {
 export async function stopDockerDatabase() {
     const yamlProjectConfiguration = await getProjectConfiguration();
     if (yamlProjectConfiguration.database?.type) {
+        let docker;
         try {
-            await asyncExec("docker --version");
+            docker = new Docker();
         } catch (error: any) {
+            log.error("An error has occured: ", error.toString());
+            log.error(
+                "\x1b[33m%s\x1b[0m",
+                "Docker not found, if you want to have a local database for testing, you need to install docker. Go to https://www.docker.com/products/docker-desktop/ to install docker",
+            );
             return undefined;
         }
-        switch (yamlProjectConfiguration.database?.type) {
-            case "postgres": {
-                await stopDockerContainer("redis", yamlProjectConfiguration.name);
-                await stopDockerContainer("postgres", yamlProjectConfiguration.name);
-                break;
-            }
-            case "redis": {
-                await stopDockerContainer("postgres", yamlProjectConfiguration.name);
-                await stopDockerContainer("redis", yamlProjectConfiguration.name);
-                break;
-            }
-            default: {
-                await stopDockerContainer("postgres", yamlProjectConfiguration.name);
-                await stopDockerContainer("redis", yamlProjectConfiguration.name);
-                log.error(
-                    "Unsupported database type, please use one of the following types of databases if you want to have a local testing db: [`postgres`,`redis`]",
-                );
-                break;
-            }
-        }
+
+        await stopDockerDatabaseContainer(docker, "postgres", yamlProjectConfiguration.name);
+        await stopDockerDatabaseContainer(docker, "redis", yamlProjectConfiguration.name);
     }
 }
 
