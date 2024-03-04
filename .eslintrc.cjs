5025e556e4baace636bbfe0f781f51bf8b3e9829
stefandaius <stefan.iordache2001@gmail.com>
2024-03-04 13:51:52 +0200

    remove no-console rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,6 @@ module.exports = {
         es6: true,
     },
     rules: {
-        "no-console": "warn",
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
