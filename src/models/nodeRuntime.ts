f685816d6b10e28dd31f4164a686a3c14a24bfe8
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2024-03-04 16:49:25 +0200

    Remove unused exported variable
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

--- src/models/nodeRuntime.ts
+++ src/models/nodeRuntime.ts
@@ -1,5 +1,4 @@
 export type NodeRuntime = "nodejs16.x" | "nodejs18.x" | "nodejs20.x";
-export const NODEJS_MAJOR_VERSIONS = ["16", "18", "20"];
 export const DEFAULT_NODE_RUNTIME: NodeRuntime = "nodejs20.x";
 export type NodeOptions = {
     nodeRuntime: NodeRuntime;
