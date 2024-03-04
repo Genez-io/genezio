c94da8eafe194962dfc93c1e41ea334baae58912
stefandaius <stefan.iordache2001@gmail.com>
2024-03-04 13:48:08 +0200

    add eslint unusedvars ignore pattern

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,4 +18,9 @@ module.exports = {
         node: true,
         es6: true,
     },
+    rules: {
+        "no-console": "warn",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
 };
