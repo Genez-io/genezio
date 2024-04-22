ad97deef987c28a51acf8c3ae10f4286a1fa0eb9
stefandaius <stefan.iordache2001@gmail.com>
2024-04-22 16:36:38 +0300

    use os specific paths

--- src/bundlers/node/typescriptRequiredDepsBundler.ts
+++ src/bundlers/node/typescriptRequiredDepsBundler.ts
@@ -1,12 +1,13 @@
 import { BundlerInput, BundlerInterface, BundlerOutput } from "../bundler.interface.js";
 import { fileExists } from "../../utils/file.js";
 import { DependencyInstaller } from "./dependencyInstaller.js";
+import path from "path";
 
 // Ensures that all dependencies required by the Typescript compiler are installed.
 export class TsRequiredDepsBundler implements BundlerInterface {
     async bundle(input: BundlerInput): Promise<BundlerOutput> {
         const cwd = input.projectConfiguration.workspace?.backend || process.cwd();
-        const exists = await fileExists(`${cwd}/node_modules/@types/node`);
+        const exists = await fileExists(path.join(cwd, "node_modules", "@types", "node"));
 
         // Install @types/node for typescript if it is not already installed
         if (!exists) {
