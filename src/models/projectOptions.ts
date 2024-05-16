b52cf4ec74ed3db4c46d4641cc6fa25a52a20829
stefandaius <stefan.iordache2001@gmail.com>
2024-05-16 12:38:53 +0300

    remove node16 and node18 runtimes

--- src/models/projectOptions.ts
+++ src/models/projectOptions.ts
@@ -1,10 +1,8 @@
-export type NodeRuntime = "nodejs16.x" | "nodejs18.x" | "nodejs20.x";
+export type NodeRuntime = "nodejs20.x";
 export type Architecture = "arm64" | "x86_64";
 export const DEFAULT_NODE_RUNTIME: NodeRuntime = "nodejs20.x";
 export const DEFAULT_ARCHITECTURE: Architecture = "arm64";
 
-export const CONTAINER_IMAGE_NODE16 = "node:16.20.2-alpine3.18";
-export const CONTAINER_IMAGE_NODE18 = "node:18.19.0-alpine";
 export const CONTAINER_IMAGE_NODE20 = "node:20.11.1-alpine3.19";
 
 export const DEFAULT_NODE_RUNTIME_IMAGE = CONTAINER_IMAGE_NODE20;
@@ -14,5 +12,5 @@ export type NodeOptions = {
     architecture: Architecture;
 };
 
-export const supportedNodeRuntimes = ["nodejs16.x", "nodejs18.x", "nodejs20.x"] as const;
+export const supportedNodeRuntimes = ["nodejs20.x"] as const;
 export const supportedArchitectures = ["arm64", "x86_64"] as const;
