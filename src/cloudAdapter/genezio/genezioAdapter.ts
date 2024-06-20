eb1d480ee7e910723cebf7bd7690fc33f9552ff9
Bogdan Vlad <vladbgd@gmail.com>
2024-06-20 16:08:36 +0300

    Ignore .github files when zipping frontend and backend.

--- src/cloudAdapter/genezio/genezioAdapter.ts
+++ src/cloudAdapter/genezio/genezioAdapter.ts
@@ -201,7 +201,10 @@ export class GenezioCloudAdapter implements CloudAdapter {
         debugLogger.debug("Creating temporary folder", archivePath);
 
         const frontendPath = path.join(frontend.path, frontend.publish || ".");
-        await zipDirectoryToDestinationPath(frontendPath, finalSubdomain, archivePath);
+        await zipDirectoryToDestinationPath(frontendPath, finalSubdomain, archivePath, [
+            ".git",
+            ".github",
+        ]);
 
         debugLogger.debug("Getting presigned URL...");
         const result = await getFrontendPresignedURL(finalSubdomain, projectName, stage);
