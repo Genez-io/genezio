0ce9e5b0b7d6dc69e6cc931ecccb7453259dd64e
Tudor Pipernea <69115828+tudorpip@users.noreply.github.com>
2024-08-21 15:23:46 +0200

    Fix pnpm for linux (#1309)
    fix: resolved pnpm compatibility for Linux function deployments

--- src/commands/deploy/genezio.ts
+++ src/commands/deploy/genezio.ts
@@ -581,6 +581,16 @@ export async function functionToCloudInput(
 
     // copy everything to the temporary folder
     await fsExtra.copy(path.join(backendPath, functionElement.path), tmpFolderPath);
+    if (fsExtra.pathExistsSync(path.join(tmpFolderPath, "node_modules", ".pnpm"))) {
+        await fsExtra.remove(path.join(tmpFolderPath, "node_modules", ".pnpm"));
+        await fsExtra.copy(
+            path.join(backendPath, functionElement.path, "node_modules", ".pnpm"),
+            path.join(tmpFolderPath, "node_modules", ".pnpm"),
+            {
+                dereference: true,
+            },
+        );
+    }
 
     const unzippedBundleSize = await getBundleFolderSizeLimit(tmpFolderPath);
 
