a56ffa95abf04882bdf64e32b38def7a50746f2b
Bogdan Vlad <vladbgd@gmail.com>
2024-09-25 11:25:31 +0300

    Remove url change.

--- src/constants.ts
+++ src/constants.ts
@@ -24,7 +24,7 @@ const NODE_MINIMUM_VERSION: string = "18.2.0";
 
 if (environment === "dev") {
     DASHBOARD_URL = "https://dev.app.genez.io";
-    BACKEND_ENDPOINT = "http://127.0.0.1:8081";
+    BACKEND_ENDPOINT = "https://dev.api.genez.io";
     LOCAL_TEST_INTERFACE_URL = "https://dev.app.genez.io/test-interface/local";
     PORT_LOCAL_ENVIRONMENT = 8083;
     ENABLE_DEBUG_LOGS_BY_DEFAULT = true;
