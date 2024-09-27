788c5b4c69debbe751b61ff414e23c4ccdb0c23a
stefandaius <stefan.iordache2001@gmail.com>
2024-09-27 14:30:36 +0300

    sanitize frontend subdomain

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -52,3 +52,20 @@ export function replaceExpression(expression: string, value: string) {
     const placeholderPattern = /\${{[A-Za-z0-9\s/.\-_]+}}/;
     return expression.replace(placeholderPattern, value);
 }
+
+/**
+ * Generate a random string of given length
+ * @param {number} length - The length of the random string to generate
+ * @returns {string} - The random string generated
+ * */
+export function randomString(length: number): string {
+    let result = "";
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
