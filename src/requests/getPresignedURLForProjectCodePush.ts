a29b1ec94b6372d950f8e9fdbaf0b65f1bfbfc5e
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2024-10-09 12:01:45 +0300

    Use GENEZIO_NOT_AUTH_ERROR_MSG instead of hardcoded string
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

--- src/requests/getPresignedURLForProjectCodePush.ts
+++ src/requests/getPresignedURLForProjectCodePush.ts
@@ -4,7 +4,7 @@ import { BACKEND_ENDPOINT } from "../constants.js";
 import version from "../utils/version.js";
 import { AxiosResponse } from "axios";
 import { StatusOk } from "./models.js";
-import { UserError } from "../errors.js";
+import { GENEZIO_NOT_AUTH_ERROR_MSG, UserError } from "../errors.js";
 
 export async function getPresignedURLForProjectCodePush(
     region = "us-east-1",
@@ -18,9 +18,7 @@ export async function getPresignedURLForProjectCodePush(
     // Check if user is authenticated
     const authToken = await getAuthToken();
     if (!authToken) {
-        throw new UserError(
-            "You are not logged in. Run 'genezio login' before you deploy your function.",
-        );
+        throw new UserError(GENEZIO_NOT_AUTH_ERROR_MSG);
     }
 
     const json = JSON.stringify({
