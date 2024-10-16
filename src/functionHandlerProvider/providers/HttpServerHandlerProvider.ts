73dfcddfb6aa5101dff8078ea75ad4867e9d2424
Bogdan Vlad <vladbgd@gmail.com>
2024-10-16 08:25:57 +0300

    Emit on server instead of calling the handler directly.

--- src/functionHandlerProvider/providers/HttpServerHandlerProvider.ts
+++ src/functionHandlerProvider/providers/HttpServerHandlerProvider.ts
@@ -15,18 +15,50 @@ const require = createRequire(import.meta.url);
 const http = require('http')
 
 const originalCreateServer = http.createServer;
-let myHandler;
+let server;
 
-http.createServer = function(options, requestListener) {
-    if (typeof options === 'function') {
-      requestListener = options;
-      options = {};
+http.createServer = function(...args) {
+  server = originalCreateServer(...args); 
+  return server
+};
+
+// Import the original app.js, but it will automatically start the server
+const app = await import("./${functionConfiguration.entry}");
+
+
+async function sendRequest(event) {
+  return new Promise(async (resolve) => {
+    const req = new http.IncomingMessage();
+    req.method = event.http.method;
+    req.url = \`\${event.http.path}\${event.url.search}\`;
+    req.headers = event.headers;
+    req.body = event.body.toString();
+    req.connection = {
+        remoteAddress: event.http.sourceIp
     }
 
-    // If options is not an object, set it to an empty object
-    options = options || {};
-  // Wrap the request listener (Express app handler) in a domain
-  const customHandler = (req, res) => {
+    const res = new http.ServerResponse(req);
+
+    res.writeHead = (status, headersLocal) => {
+        event.responseStream.writeHead(status, headersLocal);
+    }
+
+    res.write = data => {
+        event.responseStream.write(data);
+    }
+
+    res.end = data => {
+        event.responseStream.end(data);
+    }
+
+    res.status = status => {
+        event.responseStream.status(status);
+    }
+
+    res.setHeader = (name, value) => {
+        event.responseStream.setHeader(name, value);
+    }
+
     const operationId = process.domain ? process.domain.operationId : "unknown";
     const reqDomain = domain.create();
 
@@ -46,64 +78,8 @@ http.createServer = function(options, requestListener) {
     });
 
     reqDomain.run(() => {
-      requestListener(req, res); // Call the original request listener (Express app)
+      server.emit("request", req, res);
     });
-  };
-
-  myHandler = customHandler;
-
-  return originalCreateServer(myHandler);
-};
-
-// Import the original app.js, but it will automatically start the server
-const app = await import("./${functionConfiguration.entry}");
-
-
-async function getData(event) {
-  return new Promise(async (resolve) => {
-    const headers = {};
-    let statusCode = 200;
-    let body = '';
-
-    const req = new http.IncomingMessage();
-    req.method = event.http.method;
-    req.url = \`\${event.http.path}\${event.url.search}\`;
-    req.headers = event.headers;
-    req.body = event.body.toString();
-
-    const res = new http.ServerResponse(req);
-
-    res.writeHead = (status, headersLocal) => {
-        event.responseStream.writeHead(status, headersLocal);
-    }
-
-    res.write = data => {
-        event.responseStream.write(data);
-        console.log('res.write', data)
-    }
-
-    res.end = data => {
-        event.responseStream.end(data);
-        console.log('res.end', data)
-    }
-
-    res.status = status => {
-        event.responseStream.status(status);
-        console.log('res.status', status)
-    }
-
-    res.setHeader = (name, value) => {
-        console.log('res.setHeader', name, value)
-        event.responseStream.setHeader(name, value);
-    }
-
-    res.send = data => {
-        console.log('res.send', statusCode, headers, body)
-        event.responseStream.send(data);
-    }
-
-
-    await myHandler(req, res);
   }).catch((error) => {
     console.error(error);
     throw error;
@@ -111,7 +87,7 @@ async function getData(event) {
 }
 
 const handler = async function(event) {
-  await getData(event);
+  await sendRequest(event);
 };
 
 export { handler };`;
