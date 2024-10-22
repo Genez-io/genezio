21de571e18c8c4178f1640906a7ede53f973cc16
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2024-10-22 12:50:06 +0300

    Detect backend components
    Detect simple backend components such as express, fastify and serverless-http.
    This commit adds support only for js projects.
    
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

--- src/commands/analyze/frameworks.ts
+++ src/commands/analyze/frameworks.ts
@@ -1,6 +1,76 @@
 export interface PackageJSON {
+    name?: string;
+    version?: string;
+    main?: string;
+    description?: string;
+    scripts?: { [key: string]: string };
     dependencies?: { [key: string]: string };
     devDependencies?: { [key: string]: string };
+}
+
+export interface TsconfigJSON {
+    compilerOptions: {
+        target: string;
+        module: string;
+    };
+}
+
+export async function isTypescript(contents: Record<string, string>): Promise<boolean> {
+    const packageJsonContent = JSON.parse(contents["package.json"]) as PackageJSON;
+    const tsconfigJsonContent = JSON.parse(contents["tsconfig.json"]) as TsconfigJSON;
+
+    // If there is no package.json, the language is not ts, nor js
+    if (!packageJsonContent) {
+        return false;
+    }
+
+    if (tsconfigJsonContent) {
+        return true;
+    }
+
+    if (packageJsonContent.main && packageJsonContent.main.endsWith(".ts")) {
+        return true;
+    }
+
+    return false;
+}
+
+export async function getEntryfile(contents: Record<string, string>): Promise<string> {
+    // TODO Improve this - the entry file might not be defined in the package.json
+    // and it's not necessarily `index.mjs`, might be index.cjs, app.mjs, etc.
+    const packageJsonContent = JSON.parse(contents["package.json"]) as PackageJSON;
+    return packageJsonContent.main || "index.mjs";
+}
+
+// Checks if the project is a Express component
+// `contents` is a map of important file paths and their contents
+export async function isExpressBackend(contents: Record<string, string>): Promise<boolean> {
+    const packageJsonContent = JSON.parse(contents["package.json"]) as PackageJSON;
+    // If the project is a serverless-http backend, it should be treated in a different way
+    const isServerlessHttp = "serverless-http" in (packageJsonContent.dependencies || {});
+    return packageJsonContent
+        ? "express" in (packageJsonContent.dependencies || {}) && !isServerlessHttp
+        : false;
+}
+
+// Checks if the project is a Fastify component
+// `contents` is a map of important file paths and their contents
+export async function isFastifyBackend(contents: Record<string, string>): Promise<boolean> {
+    const packageJsonContent = JSON.parse(contents["package.json"]) as PackageJSON;
+    // If the project is a serverless-http backend, it should be treated in a different way
+    const isServerlessHttp = "serverless-http" in (packageJsonContent.dependencies || {});
+    return packageJsonContent
+        ? "fastify" in (packageJsonContent.dependencies || {}) && !isServerlessHttp
+        : false;
+}
+
+// Checks if the project is a serverless-http component
+// `contents` is a map of important file paths and their contents
+export async function isServerlessHttpBackend(contents: Record<string, string>): Promise<boolean> {
+    const packageJsonContent = JSON.parse(contents["package.json"]) as PackageJSON;
+    return packageJsonContent
+        ? "serverless-http" in (packageJsonContent.dependencies || {})
+        : false;
 }
 
 // Checks if the project is a Next.js component
