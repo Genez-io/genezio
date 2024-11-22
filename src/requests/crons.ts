a1ec223463d83a79d42f6a0c613ef60f8745534f
Virgil993 <turcu.virgil@gmail.com>
2024-11-22 12:43:20 +0200

    small comment

--- src/requests/crons.ts
+++ src/requests/crons.ts
@@ -1,6 +1,9 @@
 import { SyncCronsRequest, SyncCronsResponse } from "../models/requests.js";
 import sendRequest from "../utils/requests.js";
 
+// This request will start all the crons jobs sent in the request body
+// and will stop/delete all the crons that are not in the request body
+// If the request body is empty, it will stop/delete all the crons
 export async function syncCrons(request: SyncCronsRequest): Promise<SyncCronsResponse> {
     const { projectName, stageName, crons } = request;
 
