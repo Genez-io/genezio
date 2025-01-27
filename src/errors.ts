ca4ecb8d7252a1f11892caa34a0fee6d11690fdb
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2025-01-27 13:42:42 +0200

    GNZ-2213 Guide the user to run `genezio analyze` to create a `genezio.yaml`.
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

--- src/errors.ts
+++ src/errors.ts
@@ -55,7 +55,10 @@ export const GENEZIO_NOT_ENOUGH_PERMISSION_FOR_FILE = function (filePath: string
 export const GENEZIO_GIT_NOT_FOUND = `Git is not installed. Please install it and try again.
 https://git-scm.com/book/en/v2/Getting-Started-Installing-Git`;
 
-export const GENEZIO_CONFIGURATION_FILE_NOT_FOUND = `The genezio.yaml configuration file was not found. Please execute this command at the root of your project.\nIf you don't have a project yet, you can create one by running the command 'genezio create'.`;
+export const GENEZIO_CONFIGURATION_FILE_NOT_FOUND = `The \`genezio.yaml\` configuration file was not found in the specified path.
+To generate a new configuration file automatically, run \`genezio analyze\` and follow the interactive wizard.
+
+For more details, visit the documentation: ${colors.blue(`https://genezio.com/docs/cli-tool/cli-commands/analyze`)}`;
 
 export const GENEZIO_DECORATOR_YAML_OVERLAP = function (overlappingClasses: string[]) {
     const multipleOverlaps = overlappingClasses.length > 1;
